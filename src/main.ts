0ef22cd90a8c8e5686a6ace6ac08a234c02889e6
Daniel <kargapoltsev2016@gmail.com>
2026-05-24 09:54:10 +0400

    Включил валидацию глобально

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser());
   app.setGlobalPrefix('api');
 
@@ -12,10 +14,9 @@ async function bootstrap() {
     .setTitle('Crm system')
     .setDescription('Backend for Crm System')
     .setVersion('1.0')
-    .addTag('Crm')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, documentFactory);
+  SwaggerModule.setup('api/doc', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 5000);
 }
