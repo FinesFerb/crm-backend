44759a2f6c5d08a92228ef8da376aa93e299aef0
Daniel <kargapoltsev2016@gmail.com>
2026-05-24 20:31:46 +0400

    add wite list for global validate pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
   app.setGlobalPrefix('api');
 
