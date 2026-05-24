525b8a81e445001f166e4cc586ef2cfc886a3b88
Daniel <kargapoltsev2016@gmail.com>
2026-05-24 20:31:58 +0400

    add validate for .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { CommentModule } from './comment/comment.module';
 import { StorageModule } from './storage/storage.module';
 import { AuthGuard } from './auth/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import Joi from 'joi';
 
 @Global()
 @Module({
@@ -16,7 +17,19 @@ import { APP_GUARD } from '@nestjs/core';
     UserModule,
     DealModule,
     CustomerModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        R2_REGION: Joi.string().required(),
+        R2_ENDPOINT: Joi.string().required(),
+        R2_ACCESS_KEY_ID: Joi.string().required(),
+        R2_SECRET_ACCESS_KEY: Joi.string().required(),
+        R2_BUCKET_NAME: Joi.string().required(),
+        R2_PUBLIC_URL: Joi.string().required(),
+      }),
+    }),
     AuthModule,
     CommentModule,
     StorageModule,
